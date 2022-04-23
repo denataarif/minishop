506be1f4dde10ff68def8aa2bf9e55adb042a5a0
Deloyard <denataarif@gmail.com>
2022-04-23 23:18:15 +0700

    membuat footer, header, membuat store dan reducer, dan mengambil data dari API tp belum pake axios atau fecth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import Layout from './Layout/Layout'
+import Product from './Page/Product';
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<h1>Home</h1>} />
             <Route path="login" element={<h1>Login</h1>} />
-            <Route path="products" element={<h1>Products</h1>} />
+            <Route path="products" element={<Product />} />
             <Route path="catagories" element={<h1>Catagories</h1>} />
             <Route path="shoping-card" element={<h1>Shoping Card</h1>} />
             <Route path="order-history" element={<h1>Order History</h1>} />
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import App from './App';
 import './app.css'
 
@@ -7,6 +9,8 @@ import './app.css'
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
