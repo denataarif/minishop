ca30d07a1d16d66d98db56bef1fbdfc80834c7a2
Deloyard <denataarif@gmail.com>
2022-04-24 19:48:35 +0700

    membuat cart store(menambahkan product ke dalam cart)

--- src/Page/ProductDetail.js
+++ src/Page/ProductDetail.js
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import { useDispatch } from 'react-redux'
+
 const ProductDetail = () => {
 
     const [product, setProduct] = useState({})
     const params = useParams()
+
+    const dispatch = useDispatch()
+
+    const addButtonHandler = (id, title, price) => {
+        dispatch({
+            type: 'addCartItem',
+            payload: {
+                id,
+                title,
+                price
+            }
+        })
+    }
 
     useEffect(() => {
         axios.get(`https://fakestoreapi.com/products/${params.id}`)
@@ -33,7 +48,7 @@ const ProductDetail = () => {
                             <h2 className="text-2xl mb-4 font-bold">{product.title}</h2>
                             <h3 className="text-4xl text-red-500 mb-4 font-extrabold">$ {product.price}</h3>
                             <div>{product.description}</div>
-                            <button className="bg-green-500 border-0 px-6 py-2 text-white mt-8">Add To Cart</button>
+                            <button onClick={ () => addButtonHandler(product.id, product.title, product.price) } className="bg-green-500 border-0 px-6 py-2 text-white mt-8">Add To Cart</button>
                         </div>
                     </div>
                 }
